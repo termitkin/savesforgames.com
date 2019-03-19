3ce5d05ebdfad47ca60d2134a583fc2c80576f03
termitkin <bigos911@gmail.com>
2019-03-19 19:32:16 +0300

    responsive improve

--- footer.php
+++ footer.php
@@ -3,17 +3,17 @@
 	<!-- twitter scripts -->
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.async=true;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-  <footer class="footer__inner">
+  <div class="footer__inner">
 		<div class="footer__links-wrap footer__links-wrap_links-align_left">
       <a class="footer__link" href="/" title="Home page">Home</a>
       <a class="footer__link" href="//trainersforgames.com/" title="Download Trainers For Games" rel="nofollow noopener" target="_blank">Trainers For Games</a>
     </div>
 
     <div class="footer__links-wrap footer__links-wrap_links-align_right">
-      <span>Powered By <a class="footer__link" href="http://wordpress.org/" title="Powered By WordPress" rel="nofollow noopener" target="_blank">WordPress</a></span>
-      <span>&copy; 2011-2019</span>
+      <span class="footer__text">Powered By <a class="footer__link" href="http://wordpress.org/" title="Powered By WordPress" rel="nofollow noopener" target="_blank">WordPress</a></span>
+      <span class="footer__text">&copy; 2011-2019</span>
     </div>
-  </footer>
+  </div>
 
 </footer>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
