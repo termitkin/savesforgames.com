9f978643a0bbd3856002eae322c4ae002d163b66
termitkin <bigos911@gmail.com>
2019-03-19 22:20:14 +0300

    renew link to facebook sdk

--- single.php
+++ single.php
@@ -45,19 +45,9 @@
       <div class="fb-comments" data-href="<?php the_permalink() ?>" data-width="500" data-numposts="5" data-colorscheme="light"></div>
     </div>
 
-    <div id="fb-root"></div>
-    <script>
-    (function(d, s, id){
-    var js, fjs=d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js=d.createElement(s);
-    js.id=id;
-    js.async=true; js.src="//connect.facebook.net/en_US/all.js#xfbml=1&appId=289423404490301";
-    fjs.parentNode.insertBefore(js, fjs);
-    }
-    (document, 'script', 'facebook-jssdk'));
-    </script>
 
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=289423404490301"></script>
   </div>
 
   <?php get_sidebar(); ?>
