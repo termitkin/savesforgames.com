6cb76975c3ee33b260658991f7363366a67d41fa
termitkin <bigos911@gmail.com>
2019-09-03 12:27:55 +0300

    added native wordpress comments fix

--- single.php
+++ single.php
@@ -38,7 +38,12 @@
       </script>
 
       <div class="leave-a-comment">Comments:</div>
-
+      <?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+        endif;
+      ?>
     </div>
 
   </div>
