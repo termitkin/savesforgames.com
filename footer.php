f53e07164ea3814cb90c41375cdfaee007e611eb
termitkin <bigos911@gmail.com>
2022-01-06 12:49:29 +0300

    year updated

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer class="footer">
   <div class="footer__inner">
-		<div class="footer__content footer__content_align_left">
+    <div class="footer__content footer__content_align_left">
       <a class="link link_color_white" href="/" title="Home page">Home</a>
       <a class="link link_color_white" href="//trainersforgames.com/" title="Download Trainers For Games" rel="nofollow noopener" target="_blank">Trainers For Games</a>
     </div>
 
     <div class="footer__content footer__content_align_right">
       <span class="footer__text">Powered By <a class="link link_color_white" href="http://wordpress.org/" title="Powered By WordPress" rel="nofollow noopener" target="_blank">WordPress</a></span>
-      <span class="footer__text">&copy; 2011-2020</span>
+      <span class="footer__text">&copy; 2011-2022</span>
     </div>
   </div>
-
 </footer>
+
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <?php wp_footer(); ?>
 </body>
