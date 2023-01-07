7e622db78ed997677c07d8f3efbf7fa82bf14ae2
termitkin <bigos911@gmail.com>
2023-01-07 19:53:59 +0300

    Убрал adsense

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 <meta name="yandex-verification" content="3b552121d3f3e932" />
 <link rel="shortcut icon" href="/favicon.ico"/>
 <link rel="preconnect" href="https://pagead2.googlesyndication.com">
-<link rel="preconnect" href="https://googleads.g.doubleclick.net">
 <style>
 html {
   height: 100%;
@@ -457,17 +456,6 @@ body, div, header, aside, footer, input, button, ul {
       </form>
     </div>
   </header>
-
-  <div class="google-ads-in-header">
-  	<!-- savesforgames в шапке 728px -->
-  	<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-3398097190333973"
-     data-ad-slot="3640909975"></ins>
-  	<script>
-  	(adsbygoogle = window.adsbygoogle || []).push({});
-  	</script>
-  </div>
 
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
