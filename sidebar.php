da9f16eaecd28ab53de73dc82154e9306166e57f
termitkin <bigos911@gmail.com>
2023-10-11 12:52:07 +0300

    Актуализировал топ сохранений в сайдбаре

--- sidebar.php
+++ sidebar.php
@@ -19,6 +19,9 @@
 				<li>
 					<a class="link" href="/game-saves/save-for-bully-scholarship-edition/" title="Save for Bully Scholarship Edition">Bully Scholarship Edition</a>
 				</li>
+				<li>
+					<a class="link" href="/game-saves/save-for-my-summer-car/" title="Save for My Summer Car">My Summer Car</a>
+				</li>
                 <li>
                     <a class="link" href="/game-saves/save-for-assassins-creed-3/" title="Save for Assassin’s Creed 3">Assassin&apos;s Creed 3</a>
                 </li>
@@ -28,9 +31,6 @@
                 <li>
                     <a class="link" href="/game-saves/save-for-batman-arkham-city/" title="Save for Batman Arkham City">Batman Arkham City</a>
                 </li>
-				<li>
-					<a class="link" href="/game-saves/save-for-my-summer-car/" title="Save for My Summer Car">My Summer Car</a>
-				</li>
 				<li>
 					<a class="link" href="/game-saves/save-for-guitar-hero-3-legends-of-rock/" title="Save for Guitar Hero 3 Legends Of Rock">Guitar Hero 3 Legends Of Rock</a>
 				</li>
