a45d31244176712ba0dd01b17b3b9963dfab2d6b
termitkin <bigos911@gmail.com>
2023-10-11 15:11:02 +0300

    Добавил темную тему

--- header.php
+++ header.php
@@ -9,29 +9,99 @@
 <meta name="yandex-verification" content="3b552121d3f3e932" />
 <link rel="shortcut icon" href="/favicon.ico"/>
 <link rel="preconnect" href="https://www.googletagmanager.com">
+<script>
+(() => {
+    const themeName = localStorage.getItem('theme-name') || 'auto';
+
+    document.documentElement.setAttribute('data-theme-name', themeName);
+})();
+</script>
 <style>
 :root {
-  --bg-main: #eb0000;
+  --font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+
+  --bg-main: #fff;
+  --bg-secondary: #eb0000;
   --text-main: #444;
   --light-yellow: #ffc;
+  --link: var(--bg-secondary);
 
-  --pagination-default: var(--bg-main);
+  --pagination-default: var(--bg-secondary);
   --pagination-active: #ffa500;
   --pagination-hovered: #ed8000;
+
+  --search-button-bg: #fff;
+  --search-input-bg: #fff;
+
+  --logo-color-default: #fff;
+  --logo-color-hovered: var(--light-yellow);
+
+  --post-comment-button-bg: var(--bg-secondary);
+  --post-comment-button-color: #fff;
+
+  --theme-switcher-bg: var(--pagination-hovered);
 }
+@media (prefers-color-scheme: dark) {
+    :root:not([data-theme-name='light']) {
+      --bg-main: #161f2b;
+      --bg-secondary: #293645;
+      --text-main: #ddd;
+      --light-yellow: #ffc;
+      --link: #00cfff;
+
+      --pagination-default: var(--bg-secondary);
+      --pagination-active: #ffa500;
+      --pagination-hovered: #ed8000;
+
+      --search-button-bg: var(--bg-main);
+      --search-input-bg: var(--bg-main);
+
+      --logo-color-default: #fff;
+      --logo-color-hovered: var(--link);
+
+      --post-comment-button-bg: var(--bg-secondary);
+      --post-comment-button-color: #fff;
+
+      --theme-switcher-bg: var(--pagination-hovered);
+    }
+}
+:root[data-theme-name='dark'] {
+  --bg-main: #161f2b;
+  --bg-secondary: #293645;
+  --text-main: #ddd;
+  --light-yellow: #ffc;
+  --link: #00cfff;
+
+  --pagination-default: var(--bg-secondary);
+  --pagination-active: #ffa500;
+  --pagination-hovered: #ed8000;
+
+  --search-button-bg: var(--bg-main);
+  --search-input-bg: var(--bg-main);
+
+  --logo-color-default: #fff;
+  --logo-color-hovered: var(--link);
+
+  --post-comment-button-bg: var(--bg-secondary);
+  --post-comment-button-color: #fff;
+
+  --theme-switcher-bg: var(--pagination-hovered);
+}
+
 html {
   height: 100%;
 }
 body {
 	height: calc(100% - 50px);
-  font-family: Helvetica, Arial, sans-serif;
+  font-family: var(--font-family);
+  background-color: var(--bg-main);
 }
 body, div, header, aside, footer, input, button, ul {
   margin: 0;
   padding: 0;
 }
 .header {
-  background-color: var(--bg-main);
+  background-color: var(--bg-secondary);
   min-height: 40px;
   display: flex;
   justify-content: center;
@@ -57,12 +127,13 @@ body, div, header, aside, footer, input, button, ul {
   width: 260px;
   height: 30px;
   display: flex;
+  gap: 2px;
   flex-direction: row;
   justify-content: flex-end;
   align-items: center;
 }
 .site-logo {
-  color: #fff;
+  color: var(--logo-color-default);
   font-size: 1.2rem;
   font-weight: 700;
   text-decoration: none;
@@ -70,7 +141,7 @@ body, div, header, aside, footer, input, button, ul {
 }
 .site-logo:focus,
 .site-logo:hover {
-  color: var(--light-yellow);
+  color: var(--logo-color-hovered);
   text-decoration: none;
 }
 .content-and-sidebar-wrapper {
@@ -109,7 +180,7 @@ body, div, header, aside, footer, input, button, ul {
   color: var(--text-main);
 }
 .post a {
-  color: var(--bg-main);
+  color: var(--link);
 }
 .post a:hover,
 .post a:focus {
@@ -147,7 +218,7 @@ body, div, header, aside, footer, input, button, ul {
 .post-heading {
   display: inline;
   font-size: 1.125rem;
-  color: var(--bg-main);
+  color: var(--link);
   font-weight: 400;
 }
 .popular-posts {
@@ -156,7 +227,7 @@ body, div, header, aside, footer, input, button, ul {
 .popular-posts__heading {
   margin: 12px 0 3px 0;
   font-size: 1.125rem;
-  color: var(--bg-main);
+  color: var(--link);
 }
 .popular-posts__list {
   max-width: 260px;
@@ -169,15 +240,18 @@ body, div, header, aside, footer, input, button, ul {
   margin-top: 0.5rem;
 }
 .link,
+.logged-in-as a,
 .comment-reply-link,
 .comment-reply-title a {
   text-decoration: underline;
-  color: var(--bg-main);
+  color: var(--link);
   font-size: 0.8125rem;
   font-weight: 400;
 }
 .link:hover,
 .link:focus,
+.logged-in-as a:hover,
+.logged-in-as a:focus,
 .comment-reply-title a:hover,
 .comment-reply-title a:focus,
 .comment-reply-link:hover,
@@ -192,7 +266,7 @@ body, div, header, aside, footer, input, button, ul {
 }
 .footer {
   padding: 15px;
-  background-color: var(--bg-main);
+  background-color: var(--bg-secondary);
   color: #fff;
   margin-top: 20px;
   font-size: 0.875rem;
@@ -263,7 +337,7 @@ body, div, header, aside, footer, input, button, ul {
 }
 .dots:hover,
 .dots:focus {
-  background-color: var(--bg-main);
+  background-color: var(--bg-secondary);
 }
 .current,
 .current:hover,
@@ -285,7 +359,7 @@ body, div, header, aside, footer, input, button, ul {
   margin: 10px 0;
   font-size: 1.125rem;
   font-weight: 700;
-  color: var(--bg-main);
+  color: var(--link);
 }
 .fb-comments {
   min-height: 180px;
@@ -300,7 +374,7 @@ body, div, header, aside, footer, input, button, ul {
   width: 100%;
   max-width: 500px;
   margin-bottom: 14px;
-  font-family: arial, sans-serif;
+  font-family: var(--font-family);
   color: var(--text-main);
 }
 .comment-list,
@@ -318,15 +392,15 @@ body, div, header, aside, footer, input, button, ul {
 }
 .comment-body {
   padding: 10px;
-  outline: 1px solid #ddd;
-  background-color: #f5f5f5;
+  outline: 1px dashed var(--link);
+  background-color: var(--bg-main);
   border-radius: 5px;
 }
 .comment-meta {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  background-color: var(--bg-main);
+  background-color: var(--bg-secondary);
   color: #fff;
   padding: 5px;
   border-radius: 5px;
@@ -364,6 +438,7 @@ body, div, header, aside, footer, input, button, ul {
   margin: 0 0 15px 0;
   font-size: 1rem;
   line-height: 1rem;
+  color: var(--text-main);
 }
 .comment-form-comment,
 .comment-form-author {
@@ -380,12 +455,13 @@ body, div, header, aside, footer, input, button, ul {
   max-width: 500px;
   height: 30px;
   border-radius: 3px;
-  border: 1px solid #ccc;
+  border: 1px solid var(--link);
   margin: 0;
   padding: 7px;
-  background-color: #fff;
+  background-color: var(--bg-main);
   transition: background-color 0.25s;
   box-sizing: border-box;
+  color: var(--text-main);
   font-size: 0.875rem;
   line-height: 0.875rem;
 }
@@ -397,16 +473,18 @@ body, div, header, aside, footer, input, button, ul {
   min-width: 100%;
   max-width: 100%;
 }
-.input_search {
+.input_type_search {
   border: none;
   border-radius: 3px 0 0 3px;
   font-size: 0.83125rem;
   line-height: 0.83125rem;
+  background-color: var(--search-input-bg);
 }
 .submit {
   min-height: 30px;
-  background-color: #fff;
-  border: 1px solid #ccc;
+  background-color: var(--post-comment-button-bg);
+  color: var(--post-comment-button-color);
+  border: unset;
   border-radius: 3px 3px 3px 3px;
   cursor: pointer;
   padding: 7px;
@@ -416,24 +494,20 @@ body, div, header, aside, footer, input, button, ul {
 }
 .submit:hover,
 .submit:focus {
-  background-color: var(--light-yellow);
+  background-color: var(--post-comment-button-bg);
+  color: var(--post-comment-button-color);
 }
-.submit_search {
+.submit_type_search {
   height: 30px;
   border: 0;
   border-radius: 0 3px 3px 0;
-  border-left: 1px solid #ccc;
+  background-color: var(--search-button-bg);
+  color: var(--text-main);
   font-size: 0.83125rem;
   line-height: 0.83125rem;
 }
 #comment {
   min-height: 100px;
-}
-.input:focus,
-#comment:focus,
-#author:focus {
-  background-color: var(--light-yellow);
-  outline: none;
 }
 .not-found-posts {
   display: flex;
@@ -449,6 +523,35 @@ body, div, header, aside, footer, input, button, ul {
   font-size: 0.875rem;
   margin: 0;
 }
+.theme-switchers {
+    display: flex;
+    margin: 0.75rem 0;
+    align-items: center;
+    gap: 0.5rem;
+}
+@media screen and (min-width: 576px) {
+    .theme-switchers {
+        margin: 0 1rem 0 auto;
+    }
+}
+.theme-switcher {
+    width: 36px;
+    height: 36px;
+    background-color: transparent;
+    border: unset;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.15s;
+}
+.theme-switcher:hover {
+    cursor: pointer;
+    background-color: var(--theme-switcher-bg);
+}
+.theme-switcher svg {
+    fill: currentColor;
+}
 </style>
 </head>
 <body>
@@ -456,8 +559,8 @@ body, div, header, aside, footer, input, button, ul {
     <div class="header__content">
       <a class="site-logo" href="/" title="Home page">SavesForGames.Com</a>
       <form role="search" class="search-form" id="searchform" action="//savesforgames.com/" method="get">
-        <input class="input input_search" aria-label="Enter here title of a game to find a savegame" name="s" type="text" placeholder="Find a savegame..">
-        <button class="submit submit_search" aria-label="Search button" id="searchsubmit" type="submit">Search</button>
+        <input class="input input_type_search" aria-label="Enter here title of a game to find a savegame" name="s" type="text" placeholder="Find a savegame..">
+        <button class="submit submit_type_search" aria-label="Search button" id="searchsubmit" type="submit">Search</button>
       </form>
     </div>
   </header>
